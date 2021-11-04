22e35524b3bec34f7062b211e0a3d554da3940b0
Ousama Beaj <obeaj@e2r1p4.1337.ma>
2021-11-04 18:59:29 +0100

    libft without bonus

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obeaj <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/01 17:41:33 by obeaj             #+#    #+#             */
+/*   Updated: 2021/11/04 11:15:30 by obeaj            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (src[k])
+		k++;
+	while (dest[i])
+		i++;
+	if (i > size || size == 0)
+		return (k + size);
+	else
+		k += i;
+	while (src[j] && i < size - 1)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (k);
+}
