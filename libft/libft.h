23f276740ea37cd3dba8d3342e38fd832a5dd492
Ousama Beaj <obeaj@e2r9p4.1337.ma>
2021-11-04 20:46:57 +0100

    libft with half of bonus

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: obeaj <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:06:06 by obeaj             #+#    #+#             */
-/*   Updated: 2021/11/04 18:19:23 by obeaj            ###   ########.fr       */
+/*   Updated: 2021/11/04 20:37:27 by obeaj            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,5 +53,8 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const	char *s);
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
